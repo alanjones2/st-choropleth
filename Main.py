f958272a91f4a3284712d9446c630f0446973ca7
Alan Jones <jones.alan@hotmail.co.uk>
2023-12-20 20:27:04 +0100

    update main

--- Main.py
+++ Main.py
@@ -1,9 +1,20 @@
 import streamlit as st
 
-st.title("Towards a GIS using Plotly and Streamlit")
+st.set_page_config(layout="wide")
 
 st.markdown("""
-Select the pages from the sidebar to see demonstrations of the 
-various aspects of Plotly's map functions.
+# Towards a GIS using Plotly and Streamlit
+---
+            
+### Select the pages from the sidebar to see demonstrations of the various aspects of Plotly's map functions.
 
-The app is is described and documented in the tutorial [__*How to create a GIS with Plotly and Streamlit*__](#).""")
+#### The app is is described and documented in the tutorial [__*How to create a GIS with Plotly and Streamlit*__](#).
+            
+---
+""")
+
+col2, col3, col4 = st.columns(3)
+                       
+col2.image("images/1_VyS4gt40OKHc_i6FSRGIrA.png")
+col3.image("images/Screenshot 2023-03-08 194017.png")
+col4.image("images/Screenshot 2023-12-20 200434.png")
